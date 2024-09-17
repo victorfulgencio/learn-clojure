0e2de5041cc518ba71ac898d5f26b6b795d3c6f0
Victor Fulgencio <victorandradefulgencio@gmail.com>
2024-09-17 08:39:15 -0300

    using maps as schemas

--- src/mini/schema.clj
+++ src/mini/schema.clj
@@ -8,7 +8,7 @@
 (my-name-is "Victor")
 
 
-(comment "Using a record as a reference"
+(comment "Using a record as a schema"
          (s/defrecord Person [first-name :- String, last-name :- String])
 
          (s/defn name-of [person :- Person]
@@ -16,3 +16,30 @@
 
          (name-of (->Person "Victor" "Fulgencio"))
          (name-of {:first-name "Victor" :last-name "Fulgencio"}))
+
+(comment "Using a protocol as schema"
+         (defprotocol Something
+           (say-something [text]))
+
+         (defrecord DefenitlySomething [alive]
+           Something
+           (say-something [text]
+             (println "Saying something..." text)
+             alive))
+
+         (s/defn process-something :- Boolean
+           [something :- Something]
+           (say-something something))
+
+         (process-something (->DefenitlySomething true)))
+
+
+(comment "Maps as schemas"
+         (def Schema
+           {:id s/Str :keep-alive s/Bool})
+
+         (s/defn new-schema :- Schema
+           [id :- String, keep-alive :- Boolean]
+           {:id id, :keep-alive keep-alive, :hey ""})
+
+         (new-schema "abcd-123" false))
