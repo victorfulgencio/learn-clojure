911756cd5d0f9b775ea9738426b502ff2f3f892f
Victor Fulgencio <victorandradefulgencio@gmail.com>
2024-09-19 09:17:03 -0300

    add state change example

--- src/mini/state.clj
+++ src/mini/state.clj
@@ -1,1 +1,19 @@
 (ns mini.state)
+
+(defn update-state [current-state, new-item]
+  (conj current-state new-item))
+
+(defn create-item [index]
+  {:message (str "That's item number #" index)
+   :index index})
+
+(defn run-updates!
+  ([times] (run-updates! 0 times []))
+  ([index times state]
+   (if (< index times)
+     (let [new-index (inc index)
+           new-state (update-state state (create-item index))]
+       (recur new-index times new-state))
+     state)))
+
+(run-updates! 10)
